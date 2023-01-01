c6f862aa6da26251fd752ccd8f57637da579a960
Mikhail Khludnev <mikhail_khludnev@epam.com>
2023-01-02 01:44:58 +0300

    evaluate multiword synonyms

---
+++ src/test/java/org/apache/lucene/playground/TestMultiPulty.java
@@ -0,0 +1,28 @@
+package org.apache.lucene.playground;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilterFactory;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.tests.util.LuceneTestCase;
+import org.apache.lucene.util.ClasspathResourceLoader;
+
+import java.io.IOException;
+
+public class TestMultiPulty extends LuceneTestCase {
+    public void testMultyWordSynonyms() throws IOException, ParseException {
+        Analyzer ana = CustomAnalyzer.builder(new ClasspathResourceLoader(TestMultiPulty.class))
+                .withTokenizer(StandardTokenizerFactory.NAME)
+                .addTokenFilter(LowerCaseFilterFactory.NAME)
+                .addTokenFilter(SynonymGraphFilterFactory.NAME, "synonyms", "multy-syn.txt")
+                .build();
+        QueryParser dumb = new QueryParser("field", ana);
+        assertEquals("just booleans", "((+field:bot +field:net) (+field:wifi +field:router))",
+                dumb.parse("wifi router").toString());
+        assertEquals("phrases", "field:\"bot net\" field:\"wifi router\"",
+                dumb.parse("\"wifi router\"").toString());
+    }
+}
