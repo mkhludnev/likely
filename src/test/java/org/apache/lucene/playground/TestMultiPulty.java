78f25d81289cdc7b8a91ddcbd950506b551e13d4
Mikhail Khludnev <mikhail_khludnev@epam.com>
2023-01-29 23:34:50 +0300

    Reproducing Elasticsearch match_phrase

--- src/test/java/org/apache/lucene/playground/TestMultiPulty.java
+++ src/test/java/org/apache/lucene/playground/TestMultiPulty.java
@@ -7,6 +7,7 @@ import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.apache.lucene.analysis.synonym.SynonymGraphFilterFactory;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.tests.util.LuceneTestCase;
 import org.apache.lucene.util.ClasspathResourceLoader;
 
@@ -26,10 +27,21 @@ public class TestMultiPulty extends LuceneTestCase {
         assertEquals("phrases", "field:\"bot net\" field:\"wifi router\"",
                 dumb.parse("\"wifi router\"").toString());
 
+        // that's what Elasticsearch calls on match_phrase, we got disjunction of all synonymic phrases
+        final Query phraseQuery = dumb.createPhraseQuery("field", "experienced API UI jedi");
+        assertEquals("field:\"experienced application program interface user interface jedi\" " +
+                            "field:\"experienced application program interface ui jedi\" " +
+                            "field:\"experienced api user interface jedi\" " +
+                            "field:\"experienced api ui jedi\"", phraseQuery.toString());
+        // coming to standard entrypoint
         // these props seem crucial
         dumb.setSplitOnWhitespace(true);
         dumb.setAutoGeneratePhraseQueries(true);
         assertEquals("(field:\"application program interface\" field:api) (field:\"user interface\" field:ui)",
                 dumb.parse("API UI").toString());
+
+        final Query experienced_api_ui_jedi = dumb.parse("experienced API UI jedi");
+        assertEquals("field:experienced (field:\"application program interface\" field:api) (field:\"user interface\" field:ui) field:jedi",
+                experienced_api_ui_jedi.toString());
     }
 }
